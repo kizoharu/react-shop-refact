a8a3af5b047802b67e75c78933081fb74e4a5af8
kizoharu <kizoharu@gmail.com>
2022-06-10 18:02:50 +0300

    gh-pages fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <nav className='light-blue lighten-3'>
             <div className='nav-wrapper'>
-                <a href='/' className='brand-logo'>
+                <a href='/react-shop-refact' className='brand-logo'>
                     React Shop
                 </a>
                 <ul id='nav-mobile' className='right hide-on-med-and-down'>
